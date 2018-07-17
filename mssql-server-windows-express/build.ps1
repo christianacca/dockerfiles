8d572227e8d99e11b183933fce62fa8dff434544
christianacca <christian.crowhurst@gmail.com>
2018-07-17 09:41:18 +0100

    mssql-server-windows-express: build script was not pushing all tags

--- mssql-server-windows-express/build.ps1
+++ mssql-server-windows-express/build.ps1
@@ -15,28 +15,27 @@ begin {
     }
 }
 process {
+
     $osVersion = '1803'
+    $repo = 'christianacca/mssql-server-windows-express'
 
     $usCollation = 'SQL_Latin1_General_CP1_CI_AS'
     $ukCollation = 'Latin1_General_CI_AS'
-    $repo = 'christianacca/mssql-server-windows-express'
-    $usTags = 'latest', $osVersion, $usCollation, "$usCollation-$osVersion"
-    $latestImageTag = '{0}:{1}' -f $repo, ($usTags[0])
-    
-    exec { docker build -t $latestImageTag --build-arg SQL_COLLATION=$usCollation . }
-    foreach ($tag in ($usTags | Select-Object -Skip 1)) {
-        exec { docker tag $latestImageTag $repo`:$tag }
+    $builds = @{
+        $usCollation = 'latest', $osVersion, "$usCollation-$osVersion"
+        $ukCollation = "$ukCollation-$osVersion"
     }
-    
-    $ukTags = $ukCollation, "$ukCollation-$osVersion"
-    $latestUkImageTag = '{0}:{1}' -f $repo, $ukCollation
-    exec { docker build -t $latestUkImageTag --build-arg SQL_COLLATION=$ukCollation . }
-    foreach ($tag in ($ukTags | Select-Object -Skip 1)) {
-        exec { docker tag $latestUkImageTag $repo`:$tag }
+
+    foreach ($collation in $builds.Keys) {
+        $collationImageTag = '{0}:{1}' -f $repo, $collation
+        exec { docker build -t $collationImageTag --build-arg SQL_COLLATION=$collation . }
+        foreach ($tag in $builds[$collation]) {
+            exec { docker tag $collationImageTag $repo`:$tag }
+        }
     }
-    
+
     if ($Publish) {
-        foreach ($tag in $usTags) {
+        foreach ($tag in ($builds.Keys + $builds.Values)) {
             exec { docker push $repo`:$tag }
         }
     }
