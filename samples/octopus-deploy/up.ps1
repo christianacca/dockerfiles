2039c09e294453251d6763d81681fa477464ead2
christianacca <christian.crowhurst@gmail.com>
2019-02-01 19:24:27 +0000

    docker-compose octopus swarm

--- samples/octopus-deploy/up.ps1
+++ samples/octopus-deploy/up.ps1
@@ -4,7 +4,8 @@ param(
     [string] $DatabaseDockerImage = 'christianacca/mssql-server-windows-express:2017-latest-Latin1_General_CI_AS',
     [string] $OctopusVersion = '2018.12.1-1803',
     [string] $OctopusTentacleVersion = '3.22.1-1803',
-    [pscredential] $OctopusCredential
+    [pscredential] $OctopusCredential,
+    [switch] $SwarmDeploy
 )
 
 $plainTextPassword = if ($DatabasePassword) {
@@ -25,18 +26,8 @@ $env:OCTOPUS_ADMIN_PASSWORD = $OctopusCredential.GetNetworkCredential().Password
 $env:OCTOPUS_VERSION = $OctopusVersion
 $env:TENTACLE_VERSION = $OctopusTentacleVersion
 
-$volumeRootPath = "$PSScriptRoot/volumes"
-if (-not(Test-Path $volumeRootPath)) {
-    & {
-        New-Item $volumeRootPath/server/import -ItemType Directory -Force
-        New-Item $volumeRootPath/server/repository -ItemType Directory -Force
-        New-Item $volumeRootPath/server/artifacts -ItemType Directory -Force
-        New-Item $volumeRootPath/server/tasklogs -ItemType Directory -Force
-        New-Item $volumeRootPath/server/masterkey -ItemType Directory -Force
-        New-Item $volumeRootPath/server/serverlogs -ItemType Directory -Force
-        New-Item $volumeRootPath/tentacle/applications -ItemType Directory -Force
-        New-Item $volumeRootPath/tentacle/home -ItemType Directory -Force
-    } | Out-Null
+if ($SwarmDeploy) {
+    docker stack deploy -c docker-compose.yml octo
+} else {
+    docker-compose up -d    
 }
-
-docker-compose up -d
