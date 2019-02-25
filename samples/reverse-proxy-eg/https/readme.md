43ef5b9e5f4626d7eafaf6bf7606726c67185a4e
christianacca <christian.crowhurst@gmail.com>
2019-02-25 09:01:20 +0000

    sample docs

--- samples/reverse-proxy-eg/https/readme.md
+++ samples/reverse-proxy-eg/https/readme.md
@@ -1,5 +1,9 @@
-## Running sample
+## Getting started
+
+### Run sample
 
 * `up.bat`
-* browse to app: https://test-app/public-data
+* browse to app: https://app.docker.localhost
 * browse to traefic UI: http://localhost:8080
+
+To cleanup: `down.bat`
--- samples/reverse-proxy-eg/https/traefik/New-Cert.ps1
+++ samples/reverse-proxy-eg/https/traefik/New-Cert.ps1
@@ -9,10 +9,13 @@ Remove-Item certs -Force -Confirm:$false -Recurse -EA SilentlyContinue
 New-Item certs -ItemType Directory -Force
 
 # Generate ssl cert
+# For more information on how all this works see:
+# * https://medium.com/the-new-control-plane/generating-self-signed-certificates-on-windows-7812a600c2d8
+# * https://knowledge.digicert.com/solution/SO26630.html
 wsl openssl req -config docker.localhost.config -new -x509 -sha256 -newkey rsa:2048 -nodes `
     -keyout docker.localhost.key -days 3650 -out docker.localhost.crt `
     -passin pass:password1234
-wsl openssl pkcs12 -export -out docker.localhost.pfx -inkey docker.localhost.key -in docker.localhost.crt `
+    wsl openssl pkcs12 -export -out docker.localhost.pfx -inkey docker.localhost.key -in docker.localhost.crt `
     -password pass:password1234
 
 # install as a trusted root cert (so browser doesn't issue security warnings)
