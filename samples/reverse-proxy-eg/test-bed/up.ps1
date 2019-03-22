65ef582f58b04a037de4b38d4b3fa0267f3fe9cb
christianacca <christian.crowhurst@gmail.com>
2019-03-22 19:32:56 +0000

    network capture sample

--- samples/reverse-proxy-eg/test-bed/up.ps1
+++ samples/reverse-proxy-eg/test-bed/up.ps1
@@ -1,14 +1,16 @@
-docker ps -f name=test-bed_whoami  --format '{{.Names}}' | ForEach-Object {
-    $taskIdx = $_.Split('.')[1]
-    Set-Item "Env:\SEVER_CONTAINER_$taskIdx" -Value $_
-}
+docker-compose -f server-stack.yml build
+docker-compose -f client-compose.yml build
 
 docker stack deploy -c server-stack.yml test-bed
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
+docker ps -f name=test-bed_whoami  --format '{{.Names}}' | ForEach-Object {
+    $taskIdx = $_.Split('.')[1]
+    Set-Item "Env:\SERVER_CONTAINER_$taskIdx" -Value $_
+}
 docker-compose -f server-monitor-compose.yml -p server-monitor up -d
 Start-Sleep -Seconds 5
 docker-compose -f client-compose.yml -p client up -d
 Start-Sleep -Seconds 60
-docker-compose -f server-monitor-compose.yml -p server-monitor down
-docker-compose -f client-compose.yml -p client down
+docker-compose -f server-monitor-compose.yml -p server-monitor down -v
+docker-compose -f client-compose.yml -p client down -v
 docker stack rm test-bed
